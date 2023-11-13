fc54c8d746543bc28a8fc389c41e5ee9c06eb9a9
ZRNOF <ZRNOF@protonmail.com>
2023-11-13 15:05:25 +0800

    add .d.ts file

---
+++ src/main.d.ts
@@ -0,0 +1,60 @@
+/**
+ * fn cellular2D(P: vec2f) -> vec2f
+ */
+export const cellular2D: string
+
+/**
+ * fn cellular2x2(P: vec2f) -> vec2f
+ */
+export const cellular2x2: string
+
+/**
+ * fn cellular2x2x2(P: vec3f) -> vec2f
+ */
+export const cellular2x2x2: string
+
+/**
+ * fn cellular3D(P: vec3f) -> vec2f
+ */
+export const cellular3D: string
+
+/**
+ * fn cnoise2D(P: vec2f) -> f32
+ *
+ * fn pnoise2D(P: vec2f, rep: vec2f) -> f32
+ */
+export const cnoise2D: string
+
+/**
+ * fn cnoise3D(P: vec3f) -> f32
+ *
+ * fn pnoise3D(P: vec3f, rep: vec3f) -> f32
+ */
+export const cnoise3D: string
+
+/**
+ * fn cnoise4D(P: vec4f) -> f32
+ *
+ * fn pnoise4D(P: vec4f, rep: vec4f) -> f32
+ */
+export const cnoise4D: string
+
+/**
+ * fn snoise2D(v: vec2f) -> f32
+ */
+export const snoise2D: string
+
+/**
+ * fn snoise3D(v: vec3f) -> f32
+ */
+export const snoise3D: string
+
+/**
+ * fn snoise3Dgrad(v: vec3f) -> f32
+ */
+export const snoise3Dgrad: string
+
+/**
+ * fn snoise4D(v: vec4f) -> f32
+ */
+export const snoise4D: string
